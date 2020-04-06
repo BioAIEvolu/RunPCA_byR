e131008a0ebfdf4594d8c9ca88f6002b5460c5b2
BioAIEvolu <1181083735@qq.com>
2020-04-06 18:57:25 +0800

    change code

--- RunPCA.R
+++ RunPCA.R
@@ -41,27 +41,26 @@ PCA<-data.frame(PCA1=pcaPredict[, 1], PCA2=pcaPredict[, 2], group=group)
 PCA.mean<-aggregate(PCA[,1:2], by=list(group=PCA$group), FUN=mean)
 
 #¶¨ÒåÍÖÔ²
-veganCovEllipse<-function(cov, center=c(0, 0), scale=1, npoints=100) 
+veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100) 
 {
   theta <- (0:npoints) * 2 * pi/npoints
   Circle <- cbind(cos(theta), sin(theta))
   t(center + scale * t(Circle %*% chol(cov)))
 }
-
 df_ell <- data.frame()
 for(g in levels(PCA$group)){
   df_ell <- rbind(df_ell, cbind(as.data.frame(with(PCA[PCA$group==g,],
-                               veganCovEllipse(cov.wt(cbind(PCA1,PCA2),
-                               wt=rep(1/length(PCA1),length(PCA1)))$cov,
-                               center=c(mean(PCA1),mean(PCA2))))),group=g))
+                                veganCovEllipse(cov.wt(cbind(PCA1,PCA2),
+                                wt=rep(1/length(PCA1),length(PCA1)))$cov,
+                                center=c(mean(PCA1),mean(PCA2))))),group=g))
 }
 
-pdf(file='PCA2d.pdf')
-ggplot(data=PCA, aes(PCA1, PCA2)) + geom_point(aes(color=group)) +
-  geom_path(data=df_ell, aes(x=PCA1, y=PCA2, colour=group), size=1, linetype=2) + 
-  annotate("text", x=PCA.mean$PCA1, y=PCA.mean$PCA2, label=PCA.mean$group) + 
-  theme_bw() + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor=element_blank())
+pdf(file="PCA2d.pdf")
+ggplot(data = PCA, aes(PCA1, PCA2)) + geom_point(aes(color = group)) +
+  geom_path(data=df_ell, aes(x=PCA1, y=PCA2, colour=group), size=1, linetype=2)+
+  annotate("text",x=PCA.mean$PCA1,y=PCA.mean$PCA2,label=PCA.mean$group)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 dev.off()
 
 #pca 3d plot
